c539096452fd6e6ac33601d90df21cbb6a02a9e2
TDCPaciato <rizky.kurniawan@mhs.mdp.ac.id>
2022-11-10 15:14:20 +0700

    update Contact

---
+++ Contact/app/src/main/java/com/if5b/contact/activities/InputActivity.java
@@ -0,0 +1,19 @@
+package com.if5b.contact.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.if5b.contact.databinding.ActivityInputBinding;
+
+public class InputActivity extends AppCompatActivity {
+    private ActivityInputBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityInputBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+    }
+}
